3b2a52d00d9e06b3813909bf029acff58e08ffb1
Doppler-Effect <ximik827@gmail.com>
2012-11-13 23:50:31 +0400

    Added basic project structure and CORE dll

---
+++ AuLicCore/user.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AuLicCore
+{
+    public class User
+    {
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        int maxusers, currentusers;
+        string name, id;
+
+
+    }
+}
