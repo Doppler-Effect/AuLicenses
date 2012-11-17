028ac6cb2af6bff499a6323fc31bc07e4cc8981d
Doppler-Effect <ximik827@gmail.com>
2012-11-18 01:19:59 +0400

    Product activeness identification added

--- AuLicCore/licFile.cs
+++ AuLicCore/licFile.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace AuLicCore
 {
-    class licFile
+    public class licFile
     {
         public string Name
         {
@@ -16,25 +16,70 @@ namespace AuLicCore
             }
         }
         string name, path;
+        bool fileOK = false;
 
         public licFile(string name, string path)
         {
             this.path = "D:\\Dropbox\\work\\status_venera.txt";
             this.name = name;
+            fileOK = true;
         }
 
         public List<Product> findActiveProducts()
         {
-            List<Product> result = new List<Product>();
-
-            FileStream stream = new FileStream(this.path, FileMode.Open);
-            StreamReader file = new StreamReader(stream);
-            while (!file.EndOfStream)
+            if (fileOK)
             {
-                string row = file.ReadLine();
-                
+                List<Product> result = new List<Product>();
+
+                FileStream stream = new FileStream(this.path, FileMode.Open);
+                StreamReader file = new StreamReader(stream);
+                while (!file.EndOfStream)
+                {
+                    string row = file.ReadLine();
+                    if (row.Contains("Users of") && this.isProductActive(row))
+                    {
+                        result.Add(new Product(row));
+                    }
+                }
+                stream.Close();
+                return result;
             }
+            else
+                return null;
         }
-        
+
+        bool isProductActive(string row)
+        {
+            int pos = row.LastIndexOf("Total of");
+            int licCount = getNumberFromPosition(pos, row);
+            if (licCount > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static int getNumberFromPosition(int pos, string row)
+        {
+            int result;
+            string str = "";
+
+            while (!char.IsNumber(row, pos))
+            {
+                pos += 1;
+            }
+            do
+            {
+                str += row[pos];
+                pos += 1;
+            } while (char.IsNumber(row, pos)) ;
+
+            str = str.Trim();
+
+            if(int.TryParse(str, out result))
+                return result;
+            else
+                return 0;
+        }
+
     }
 }
---
+++ testLicProj/testProg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AuLicCore;
+
+namespace testLicProj
+{
+    class testProg
+    {
+        static void Main(string[] args)
+        {
+            licFile file = new licFile("File1", "foo");
+            file.findActiveProducts();
+        }
+    }
+}
